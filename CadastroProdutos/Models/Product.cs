ead1826ef8db13f23b5e2d30ee6384923bd32d16
Rafael Bitu Castro de Abreu <rafael.bitu@dinfor.uniceub>
2025-10-31 10:44:20 -0300

    GUI

---
+++ CadastroProdutos/Models/Product.cs
@@ -0,0 +1,9 @@
+namespace CadastroProdutos.Models;
+
+public class Product {
+    public string Name{get;set;} = "";
+    public string Category{get;set;} = "";
+    public float Price{get;set;} = 0;
+    public int Quantity{get;set;} = 0;
+
+}
